fd095e9b8d7a37ab433b585734ec1a5c7df9ec5f
wardbradt <wardbradt5@gmail.com>
2020-06-04 13:30:37 -0400

    make this.state.values a string array

--- client/src/components/golleInput.jsx
+++ client/src/components/golleInput.jsx
@@ -3,11 +3,15 @@ import React, {Component} from "react";
 export default class GolleInput extends Component {
     constructor(props) {
         super(props);
-        this.state = {values: this.props.values}
+        this.state = {values: []}
+        for (let i = 0; i < this.props.values; i++) {
+            this.state.values.push('');
+        }
         this.handleChange = this.handleChange.bind(this);
         this.handleKeydown = this.handleKeydown.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.onSetSuccessfully = this.onSetSuccessfully.bind(this);
+        this.numberInput = this.numberInput.bind(this);
     }
     handleChange(ind, event) {
         this.setState(prevState => ({values: [...prevState.values.slice(0, ind), event.target.value, ...prevState.values.slice(ind + 1)]}));
@@ -18,19 +22,27 @@ export default class GolleInput extends Component {
         }
     }
     handleSubmit() {
-        for (let value of this.state.values) {
-            if (!parseInt(value) && value !== 0) {
-                alert('To customize Golle values, please input an integer.');
-                return;
-            } else if (value < 0) {
-                alert(`Golle values must be between 0 and 51. ${value} is not.`);
-                return;
-            } else if (value > 51) {
-                alert(`Golle values must be between 0 and 51. ${value} is not.`);
-                return;
+        let newValues = [];
+        for (let i = 0; i < this.state.values.length; i++) {
+            let value = this.state.values[i];
+            if (value === '') {
+                newValues.push(this.props.values[i]);
+            } else {
+                if (!parseInt(value) && value !== 0) {
+                    alert('To customize Golle values, please input an integer.');
+                    return;
+                } else if (value < 0) {
+                    alert(`Golle values must be between 0 and 51. ${value} is not.`);
+                    return;
+                } else if (value > 51) {
+                    alert(`Golle values must be between 0 and 51. ${value} is not.`);
+                    return;
+                }
+                // Only update changed values
+                newValues.push(this.state.values[i]);
             }
         }
-        this.props.socket.emit('setGolleNumbers', {values: this.state.values});
+        this.props.socket.emit('setGolleNumbers', {values: newValues});
     }
     onSetSuccessfully(data) {
         this.setState({values: data.values});
@@ -49,10 +61,16 @@ export default class GolleInput extends Component {
     componentWillUnmount() {
         this.props.socket.off('setGolleNumbers', this.onSetSuccessfully);
     }
+    numberInput(val, ind) {
+        let className = "inputVal two columns golleInput";
+        let id = `golle-${ind}`;
+        const onChange = (e) => {this.handleChange(ind, e)};
+        return <input key={ind} type="number" placeholder={val} className={className} id={id} value={this.state.values[ind]} onChange={onChange}/>
+    }
     render() {
         return (
             <div className="row pad u-full-width">
-                {this.state.values.map((val, ind) => <input key={ind} type="number" className="inputVal two columns golleInput" id={`golle-${ind}`} value={this.state.values[ind]} onChange={(e) => {this.handleChange(ind, e)}}/>)}
+                {this.props.values.map(this.numberInput)}
                 <button className="button-primary four columns" id="golle-update" onClick={this.handleSubmit}>Update</button>
             </div>
         );
